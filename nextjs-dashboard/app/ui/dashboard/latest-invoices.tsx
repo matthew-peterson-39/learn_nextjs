c2d140317dd72e33ad7dd219b8b8fdc2eec40ec0
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2024-01-13 14:50:45 -0500

    Add Card data

--- nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
+++ nextjs-dashboard/app/ui/dashboard/latest-invoices.tsx
@@ -1,9 +1,10 @@
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
+import { lusitana } from '../fonts';
+import { LatestInvoice } from '../../lib/definitions';
 import React from 'react';
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
