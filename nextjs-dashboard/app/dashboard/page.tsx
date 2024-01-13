c0ab695a718ff458da769e18e00b6cba8c264445
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2024-01-13 14:33:29 -0500

    Add latest invoices

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import { Card } from '../ui/dashboard/cards';
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts';
-import { fetchRevenue } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices } from '../lib/data';
+import React from 'react';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+    
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
@@ -19,9 +22,9 @@ export default async function Page() {
                     type="customers"
                 /> */}
             </div>
-            <div className='mt-6 grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+            <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
                 <RevenueChart revenue={revenue}  />
-                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
 
         </main>
